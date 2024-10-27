4d1f8eb09f7c9077ae6eaaf2249a095490587925
Rin Pichphyrom <157264890+RomNeedBoba@users.noreply.github.com>
2024-10-27 22:35:49 +0700

    Create main.dart

---
+++ Week 3/Exer1/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: Text(
+          "Real Slim Shady!!!!!!!!",
+            style: TextStyle(
+              color: Colors.orange,
+              fontSize: 50,
+        ),
+      )),
+    ),
+  );
+}
