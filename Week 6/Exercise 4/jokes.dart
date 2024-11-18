247efed4192dacf2def8608f2ff6aedb66d158e4
Anonymous Tech Nerd <157264890+RomNeedBoba@users.noreply.github.com>
2024-11-18 13:34:21 +0700

    Update jokes.dart

--- Week 6/Exercise 4/jokes.dart
+++ Week 6/Exercise 4/jokes.dart
@@ -19,4 +19,72 @@ List<Joke> jokes = [
     title: "Apateu",
     description: "uh huh huh huh huh",
   ),
+    Joke(
+    title: "What’s orange and sounds like a carrot?",
+    description: "a parrot",
+  ),
+    Joke(
+    title: "What do you call a woman with one leg?",
+    description: "Eileen.",
+  ),
+    Joke(
+    title: "Why did the frog take the bus to work today?",
+    description: "His car got toad away",
+  ),
+    Joke(
+    title: "Why did the yogurt go to the art exhibition?",
+    description: "Because it was cultured.",
+  ),
+    Joke(
+    title: "What do you call an apology written in dots and dashes?",
+    description: "Re-Morse code.",
+  ),
+    Joke(
+    title: "Why do French people eat snails?",
+    description: "They don’t like fast food.",
+  ),
+    Joke(
+    title: "Why don’t the circus lions eat the clowns? ",
+    description: "Because they taste funny!,
+  ),
+    Joke(
+    title: "How many tickles does it take to make an octopus laugh?",
+    description: "Ten-tickles",
+  ),
+    Joke(
+    title: "What did 0 say to 8?",
+    description: "Nice belt.",
+  ),
+    Joke(
+    title: "What do you call a pig that does karate?",
+    description: "A pork chop.",
+  ),
+    Joke(
+    title: "Why are elephants wrinkly?",
+    description: "Because you can’t iron them.",
+  ),
+    Joke(
+    title: "AWhat did the cake say to the fork?",
+    description: "You want a piece of me?",
+  ),
+    Joke(
+    title: "Why is it annoying to eat next to basketball players?",
+    description: "They dribble all the time.",
+  ),
+    Joke(
+    title: "What did the lettuce say to the celery?",
+    description: "Quit stalking me!",
+  ),
+    Joke(
+    title: "What do you call a train carrying bubblegum?",
+    description: "A chew-chew train",
+  ),
+    Joke(
+    title: "How do trees get online?",
+    description: "They just log on!",
+  ),
+    Joke(
+    title: "What do you call a bear with no teeth?",
+    description: "A gummy bear.",
+  ),
 ];
