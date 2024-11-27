c238903de002e59af5b67f2251bbfc95473ac80a
Anonymous Tech Nerd <157264890+RomNeedBoba@users.noreply.github.com>
2024-11-27 21:15:24 +0700

    Update quiz_app.dart

--- Week 7/Week 7 S2/lib/quiz_app.dart
+++ Week 7/Week 7 S2/lib/quiz_app.dart
@@ -1,32 +1,72 @@
 import 'package:flutter/material.dart';
 import 'model/quiz.dart';
+import 'model/submission.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/question_screen.dart';
+import 'screens/result_screen.dart';
 
-Color appColor = Colors.blue[500] as Color;
- 
+Color appColor = Colors.blue;
+
+enum QuizState { notStarted, started, finished }
+
 class QuizApp extends StatefulWidget {
-  const QuizApp(this.quiz, {super.key});
+  const QuizApp(this.quiz, {Key? key}) : super(key: key);
 
   final Quiz quiz;
 
   @override
-  State<QuizApp> createState() => _QuizAppState();
+  State<QuizApp> createState() => QuizAppState();
 }
 
-class _QuizAppState extends State<QuizApp> {
-  
+class QuizAppState extends State<QuizApp> {
+  QuizState quizState = QuizState.notStarted;
+  Submission submission = Submission();
+
+  void startQuiz() {
+    setState(() {
+      quizState = QuizState.started;
+    });
+  }
+
+  void finishQuiz() {
+    setState(() {
+      quizState = QuizState.finished;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget currentScreen;
+
+    if (quizState == QuizState.notStarted) {
+      currentScreen = WelcomeScreen(
+        onStart: startQuiz,
+        quizTitle: widget.quiz.title,
+      );
+    } else if (quizState == QuizState.started) {
+      currentScreen = QuestionScreen(
+        onFinish: finishQuiz,
+        quiz: widget.quiz,
+        currentQuestionIndex: 0,
+        submission: submission,
+      );
+    } else {
+      currentScreen = ResultScreen(
+        submission: submission,
+        quiz: widget.quiz,
+        onRestart: () {
+          setState(() {
+            quizState = QuizState.notStarted;
+            submission.removeAnswers();
+          });
+        },
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: appColor,
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('TODO !'),
-            ],
-          ),
-        ),
+        body: Center(child: currentScreen),
       ),
     );
   }
